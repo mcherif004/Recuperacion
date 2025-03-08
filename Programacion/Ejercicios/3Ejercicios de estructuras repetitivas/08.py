9a67e3d4f2ae906e72dc8fdacef6935321165e93
mcherif04 <mostafacherif421@gmail.com>
2025-03-08 10:50:27 +0100

    Update Enunciados

--- Programacion/Ejercicios/3Ejercicios de estructuras repetitivas/08.py
+++ Programacion/Ejercicios/3Ejercicios de estructuras repetitivas/08.py
@@ -1,12 +1,1 @@
-# Programa que pida dos números ‘nota’ y ‘edad’ y un carácter ‘sexo’ y muestre el mensaje ‘ACEPTADA’ si la nota es mayor o igual a cinco, la edad es mayor o igual a dieciocho y el sexo es ‘F’. En caso de que se cumpla lo mismo, pero el sexo sea ‘M’, debe imprimir ‘POSIBLE’. Si no se cumplen dichas condiciones se debe mostrar ‘NO ACEPTADA’.
-
-nota = float(input("Introduce la nota: "))
-edad = int(input("Introduce la edad: "))
-sexo = input("Introuduce el sexo (M/F): ")
-
-if nota >=5 and edad >= 18 and sexo == "F":
-    print("ACEPTADA")
-elif nota >=5 and edad >= 18 and sexo == "M":
-    print("POSIBLE")
-else:
-    print("NO ACEPTADA")
+# Hacer un programa que muestre un cronometro, indicando las horas, minutos y segundos. Para hacer una espera en Python podemos usar el método sleep del módulo time.
